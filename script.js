e05270f3092900f13b013f6fc4711b2b490c5345
Wistful-max <matia013@osloskolen.no>
2026-09-10 12:46:05 +0200

    made random work
    You can get a random thing from the list by clicking and you cant select the test button.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-conts question = ["question0", "question1","question2"]
-Math.floor(Math.random() * 11);
+const question = ["question0", "question1","question2"];
+
+const please = document.getElementById("test")
+please.addEventListener('click',function(){
+    document.getElementById("test").textContent = question[Math.floor(Math.random() * 2)]
+})
+
+quesstionMath.floor(Math.random() * 11);

187af781d883ed33d1c306d06424bcb6bc9e65d2
Wistful-max <matia013@osloskolen.no>
2026-09-10 13:49:15 +0200

    made an object in an array to pull things from
    Now you can pull a set of different things at once.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const question = ["question0", "question1","question2"];
+const question = ["q0", "q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"];
+
+const question_set = [
+    {
+    text: "Hvor mange prosent er 3 stjerner i COC?",
+    correct: 3,
+    answersCOC: ["50","1","100","85-100"],
+    }
+]
 
 const please = document.getElementById("test")
 please.addEventListener('click',function(){
-    document.getElementById("test").textContent = question[Math.floor(Math.random() * 2)]
+    document.getElementById("test").textContent = question_set[0].text
 })
 
-quesstionMath.floor(Math.random() * 11);
+Math.floor(Math.random() * 11);
+
